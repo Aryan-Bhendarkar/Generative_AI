a715d4d230db8f2fc15e0f9943beaef23db1c8cb
Aryan Bhendarkar <bhendarkararyan@gmail.com>
2026-05-16 17:31:53 +0530

    Build the Reseach Paper Summarizer using dynamic prompting technique with the help of langchain promptTemplate

---
+++ LangChain_Prompts/dynamic_research.py
@@ -0,0 +1,25 @@
+from langchain_openrouter import ChatOpenRouter
+from langchain_core.prompts import PromptTemplate, load_prompt
+from dotenv import load_dotenv
+import streamlit as st
+
+load_dotenv()
+model = ChatOpenRouter(model='openai/gpt-4o-mini')
+
+st.header('Reasearch Tool')
+
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+# template = PromptTemplate()
+template = load_prompt('template.json')
+
+if(st.button('summarize')):
+    chain = template | model
+    result = chain.invoke({
+        'paper_input':paper_input,
+        'style_input':style_input,
+        'length_input':length_input
+    })
+    st.write(result.content)
