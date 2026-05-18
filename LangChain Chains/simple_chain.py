a76f2b8b92bcf12b441a33c33c745ba63c329d3f
Aryan Bhendarkar <bhendarkararyan@gmail.com>
2026-05-18 15:08:43 +0530

    Implemented Simple Chains using LangChain

---
+++ LangChain Chains/simple_chain.py
@@ -0,0 +1,21 @@
+from langchain_openrouter import ChatOpenRouter
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+load_dotenv()
+
+prompt = PromptTemplate(
+    template="Generate 5 interesting things about {topic}",
+    input_variables=['topic']
+)
+
+model = ChatOpenRouter(model='openai/gpt-4o-mini')
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+result = chain.invoke({'topic':'cricket'})
+print(result)
+
+chain.get_graph().print_ascii()
