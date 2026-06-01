3114f2f2ee120b909c8de67c0e665688a340543e
Aryan Bhendarkar <bhendarkararyan@gmail.com>
2026-06-01 14:23:25 +0530

    Add search and weather tools with OpenRouter LLM setup

---
+++ LangChain Agents/agent.py
@@ -0,0 +1,19 @@
+from langchain_openrouter import ChatOpenRouter
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain.tools import tool
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+search_tool = DuckDuckGoSearchRun()
+result = search_tool.invoke("today's top News in India ")
+
+@tool
+def get_weather(city: str) -> str:
+    """This tool fetches the current weather data form a given city"""
+    url = f"http://api.weatherstack.com/current?access_key=df739feb8122522cc2cdbae91af53a80&query={city}"
+    response = requests.get(url)
+    return response.json()
+
+llm = ChatOpenRouter(model = "openai/gpt-4o-mini")
